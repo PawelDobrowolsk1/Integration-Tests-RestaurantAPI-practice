72212c7a1a0b8c60190383d44850bc7d2f9da899
PawelDobrowolsk1 <pdobrowolski001@gmail.com>
2023-03-27 00:04:08 +0200

    Added a dependency registration test

---
+++ RestaurantAPI.IntegrationTests/ProgramTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RestaurantAPI.IntegrationTests
+{
+    public class ProgramTests
+    {
+        private readonly List<Type> _controllerTypes;
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public ProgramTests()
+        {
+            _controllerTypes = typeof(Program)
+                .Assembly
+                .GetTypes()
+                .Where(t => t.IsSubclassOf(typeof(ControllerBase)))
+                .ToList();
+
+            _factory  = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        _controllerTypes.ForEach(c => services.AddScoped(c));
+                    });
+                });
+        }
+        [Fact]
+        public void ConfigureServices_ForControllers_RegistersAllDependencies()
+        {
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+
+            // assert
+            _controllerTypes.ForEach(t =>
+            {
+                var controller = scope.ServiceProvider.GetService(t);
+                controller.Should().NotBeNull();
+            });
+
+        }
+    }
+}
