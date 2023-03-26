6f4af7b859b8f91a45e5ac92fb08508fd1bd064d
PawelDobrowolsk1 <pdobrowolski001@gmail.com>
2023-03-26 17:04:02 +0200

    Added test which required authentication

--- RestaurantAPI.IntegrationTests/RestaurantControllerTests.cs
+++ RestaurantAPI.IntegrationTests/RestaurantControllerTests.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.AspNetCore.Authorization.Policy;
 
 namespace RestaurantAPI.IntegrationTests 
 {
@@ -22,6 +26,10 @@ namespace RestaurantAPI.IntegrationTests
 
                         services.Remove(dbContextOptions);
 
+                        services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluatro>();
+
+                        services.AddMvc(options => options.Filters.Add(new FakeUserFilter()));
+
                         services
                             .AddDbContext<RestaurantDbContext>(options => options.UseInMemoryDatabase("RestaurantDb"));
                     });
@@ -29,6 +37,37 @@ namespace RestaurantAPI.IntegrationTests
 
             _client = factory.CreateClient();
         }
+
+        [Fact]
+        public async Task CreateRestaurant_WithValidModel_ReturnsCreatedStatus()
+        {
+            // arrange
+            var model = new CreateRestaurantDto()
+            {
+                Name = "TestRestaurant",
+                Description = "Description",
+                Category = "Test",
+                HasDelivery = true,
+                ContactEmail = "TestEmail",
+                ContactNumber = "TestNubmer",
+                City= "TestCity",
+                Street = "TestStreet",
+                PostalCode = "TestPostalCode"
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+
+            var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+
+            //act
+            var response = await _client.PostAsync("/api/restaurant", httpContent);
+
+            // assert
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+            response.Headers.Location.Should().NotBeNull();
+        }
+
 
         [Theory]
         [InlineData("pageSize=5&pageNumber=1")]
