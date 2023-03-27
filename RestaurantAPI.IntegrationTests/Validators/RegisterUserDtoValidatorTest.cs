671c89a1431decc863ed00e10b8267b914fd8341
PawelDobrowolsk1 <pdobrowolski001@gmail.com>
2023-03-27 13:10:31 +0200

    Added validator testing

---
+++ RestaurantAPI.IntegrationTests/Validators/RegisterUserDtoValidatorTest.cs
@@ -0,0 +1,89 @@
+﻿using FluentValidation.TestHelper;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Bson;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+using RestaurantAPI.Models.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace RestaurantAPI.IntegrationTests.Validators
+{
+    public class RegisterUserDtoValidatorTest
+    {
+        private RestaurantDbContext _dbContext;
+
+        public RegisterUserDtoValidatorTest()
+        {
+            var builder = new DbContextOptionsBuilder<RestaurantDbContext>();
+            builder.UseInMemoryDatabase("TestDb");
+
+            _dbContext = new RestaurantDbContext(builder.Options);
+            Seed();
+        }
+
+        public void Seed()
+        {
+            var testUsers = new List<User>()
+            {
+                new User()
+                {
+                    Email = "test2@test.com"
+                },
+                new User()
+                {
+                    Email = "test3@test.com"
+                }
+            };
+            _dbContext.Users.AddRange(testUsers);
+            _dbContext.SaveChanges();
+        }
+
+        [Fact]
+        public void Validate_ForValidModel_ReturnsSuccess()
+        {
+            // arrange
+
+            var model = new RegisterUserDto()
+            {
+                Email = "test@test.com",
+                Password = "password123",
+                ConfirmPassword = "password123",
+            };
+
+            var validator = new RegisterUserDtoValidator(_dbContext);
+
+            // act 
+            var result = validator.TestValidate(model);
+
+            // assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void Validate_ForInvalidModel_ReturnsFailure()
+        {
+            // arrange
+
+            var model = new RegisterUserDto()
+            {
+                Email = "test2@test.com",
+                Password = "password123",
+                ConfirmPassword = "password123",
+            };
+
+            var validator = new RegisterUserDtoValidator(_dbContext);
+
+            // act 
+            var result = validator.TestValidate(model);
+
+            // assert
+            result.ShouldHaveAnyValidationError();
+        }
+    }
+}
---
+++ RestaurantAPI.IntegrationTests/Validators/RestaurantQueryValidatorTest.cs
@@ -0,0 +1,115 @@
+﻿using FluentValidation.TestHelper;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+using RestaurantAPI.Models.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RestaurantAPI.IntegrationTests.Validators
+{
+    public class RestaurantQueryValidatorTest
+    {
+        public static IEnumerable<object[]> GetSampleValidData()
+        {
+            var list = new List<RestaurantQuery>()
+            {
+                new RestaurantQuery()
+                {
+                    PageNumber = 1,
+                    PageSize = 10,
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber = 2,
+                    PageSize = 15,
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber = 3,
+                    PageSize = 5,
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber = 2,
+                    PageSize = 5,
+                    SortBy =  nameof(Restaurant.Name)
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber = 12,
+                    PageSize = 15,
+                    SortBy =  nameof(Restaurant.Category)
+                },
+            };
+
+            return list.Select(q => new object[] { q });
+        }
+        public static IEnumerable<object[]> GetSampleInvalidData()
+        {
+            var list = new List<RestaurantQuery>()
+            {
+                new RestaurantQuery()
+                {
+                    PageNumber = -1,
+                    PageSize = 10,
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber = 2,
+                    PageSize = 12,
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber = -3,
+                    PageSize = -5,
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber = 2,
+                    PageSize = 5,
+                    SortBy =  nameof(Restaurant.ContactEmail)
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber = 12,
+                    PageSize = 15,
+                    SortBy =  nameof(Restaurant.ContactNumber)
+                },
+            };
+
+            return list.Select(q => new object[] { q });
+        }
+
+        [Theory]
+        [MemberData(nameof(GetSampleValidData))]
+        public void Validate_ForCorrectModel_ReturnsSuccess(RestaurantQuery model)
+        {
+            // arrange
+            var validator = new RestaurantQueryValidator();
+
+            // act 
+            var result = validator.TestValidate(model);
+
+            // assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Theory]
+        [MemberData(nameof(GetSampleInvalidData))]
+        public void Validate_ForIncorrectModel_ReturnsFailure(RestaurantQuery model)
+        {
+            // arrange
+            var validator = new RestaurantQueryValidator();
+
+            // act 
+            var result = validator.TestValidate(model);
+
+            // assert
+            result.ShouldHaveAnyValidationError();
+        }
+    }
+}
